3eafed1f836b6ba0abc032d36f1be4770b92659e
Milena Govor <milena.govorr@gmail.com>
2023-11-26 21:05:28 +0300

    add code to funcs.h

---
+++ funcs.h
@@ -0,0 +1,12 @@
+//function.h
+
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+class Function {
+public:
+    static double add(double a, double b);
+    static double subtract(double a, double b);
+};
+
+#endif // FUNCTION_H
